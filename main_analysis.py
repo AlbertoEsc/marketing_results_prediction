ec3f7585debc991b2658777033d10e64f9686ee9
Alberto Escalante <alberto.escalante@ini.rub.de>
2018-08-12 13:45:44 +0200

    Divides the data into train, validation, and test sets

--- main_analysis.py
+++ main_analysis.py
@@ -18,6 +18,7 @@ import pandas as pd
 import numpy as np
 import sklearn as sk
 from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 from null_handling import remove_nulls
@@ -59,9 +60,26 @@ print('pca.explained_variance_:', pca.explained_variance_)
 print('pca.components_:', pca.components_)
 print('pca.components_[0]:', pca.components_[0])
 
-# Label enlargement
+lead_id = data_np[:, 0]
+target_sold = data_np[:, 1]
+target_sales = data_np[:, 2]
+data_np = data_np[:, 3:]
+
+data_np_sold = data_np[target_sold == 1.0, :]
+target_sales_sold = target_sales[target_sold == 1.0]
+print("len(data_np_sold):", len(data_np_sold), "len(target_sales_sold):", len(target_sales_sold))# Label enlargement
 
 # Split of samples
+RANDOM_STATE_TEST = 1234
+RANDOM_STATE_VAL = 5678
+X_train, X_test, ts_train, ts_test = \
+        train_test_split(data_np, target_sold, test_size=0.20,
+                         random_state=RANDOM_STATE_TEST)
+X_train, X_val, ts_train, ts_val = \
+        train_test_split(X_train, ts_train, test_size=0.25,
+                         random_state=RANDOM_STATE_VAL)
+print("len(X_train):", len(X_train), "len(X_val):", len(X_val), "len(X_test):", len(X_test))
+print("len(ts_train):", len(ts_train), "len(ts_val):", len(ts_val), "len(ts_test):", len(ts_test))
 
 
 # ML methods
