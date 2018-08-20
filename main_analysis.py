8497904cd5d115c97dff81ec6f193f256763e87e
Alberto Escalante <alberto.escalante@ini.rub.de>
2018-08-20 23:32:29 +0200

    Adds a third model that directly computes the expected return

--- main_analysis.py
+++ main_analysis.py
@@ -64,9 +64,9 @@ enable_pca_analysis = False
 enable_random_forest_classifier = True
 enable_gradient_boosting_classifier = False # or True
 
-enable_ridge_regression = True
+enable_ridge_regression = False #or True
 enable_random_forest_regressor = False or True
-enable_gradient_boosting_regressor = False or True
+enable_gradient_boosting_regressor = False #or True
 
 # Load data and field explanations (files have been renamed)
 data_df, id_df, target_sold_df, target_sales_df = read_data(filename)
@@ -115,12 +115,21 @@ X_train, X_val, X_test = \
 y_train, y_val, y_test = \
     split_data(target_sold, indices_train, indices_val, indices_test)
 
+# Data for second model
 X_m2_train = X_train[y_train == 1]
 X_m2_val = X_val[y_val == 1]
 X_m2_test = X_test[y_test == 1]
 y_m2_train = target_sales_all[indices_train][y_train == 1]
 y_m2_val = target_sales_all[indices_val][y_val == 1]
 y_m2_test = target_sales_all[indices_test][y_test == 1]
+
+# Data for third model
+X_m3_train = X_train
+X_m3_val = X_val
+X_m3_test = X_test
+y_m3_train = target_sales_all[indices_train]
+y_m3_val = target_sales_all[indices_val]
+y_m3_test = target_sales_all[indices_test]
 
 # TODO: consider aligning the data for both models
 # RANDOM_STATE_TEST = 12
@@ -173,7 +182,9 @@ X_m2_test =  (X_m2_test - X_train_mean) / X_train_std
 # Feature normalization (zero-mean unit-variance or range [0, 1])
 
 # ML methods
+# First model (M1). Estimation of whether a lead will result in 'sold'
 test_acc = {}
+classification_methods = []
 # M1. ChanceLevel estimations
 test_acc[('ChanceLevel_MSE', 'train')] = mean_squared_error(y_train, y_train.mean() * np.ones_like(y_train))
 test_acc[('ChanceLevel_MSE', 'val')] = mean_squared_error(y_val, y_val.mean() * np.ones_like(y_val))
@@ -196,6 +207,7 @@ test_acc[('LinearRegression_CR', 'val')] = accuracy_score(y_val, pred_lr_val >= 
 test_acc[('LinearRegression_CE', 'train')] = cross_entropy(y_train, prob_lr_train)
 test_acc[('LinearRegression_CE', 'val')] = cross_entropy(y_val, prob_lr_val)
 print(lr.coef_)
+classification_methods.append('lr')
 
 # M1. LogisticRegression
 logr = LogisticRegression(C=1.0)
@@ -210,6 +222,7 @@ test_acc[('LogisticRegression_CR', 'train')] = accuracy_score(y_train, pred_logr
 test_acc[('LogisticRegression_CR', 'val')] = accuracy_score(y_val, pred_logr_val)
 test_acc[('LogisticRegression_CE', 'train')] = cross_entropy(y_train, prob_logr_train)
 test_acc[('LogisticRegression_CE', 'val')] = cross_entropy(y_val, prob_logr_val)
+classification_methods.append('logr')
 
 # M1. RandomForestClassifier
 if enable_random_forest_classifier:
@@ -229,6 +242,7 @@ if enable_random_forest_classifier:
     test_acc[('RandomForestClassifier_CE', 'val')] = cross_entropy(y_val, prob_rf_val)
     print("RandomForest best_score:", rf_rs.best_score_,
           "best_params:", rf_rs.best_params_)
+    classification_methods.append('rfc')
 
 # M1. GradientBoostingClassifier
 if enable_gradient_boosting_classifier:
@@ -255,6 +269,7 @@ if enable_gradient_boosting_classifier:
 
     print("GradientBoostingClassifier best_score:", gbc_rs.best_score_,
           "best_params:", gbc_rs.best_params_)
+    classification_methods.append('gbc')
 
 print(test_acc)
 
@@ -263,21 +278,24 @@ for (alg, test_set) in test_acc.keys():
     res_sold_df.loc[alg, test_set] = test_acc[(alg, test_set)]
 print(res_sold_df)
 
-# Second model (M2)
+# Second model (M2). Estimation of sales for successful leads
 test_m2_acc = {}
+regression_methods = []
+# Chance levels
 test_m2_acc[('ChanceLevel_MSE', 'train')] = \
     mean_squared_error(y_m2_train, y_m2_train.mean() * np.ones_like(y_m2_train))
 test_m2_acc[('ChanceLevel_MSE', 'val')] = \
     mean_squared_error(y_m2_val, y_m2_val.mean() * np.ones_like(y_m2_val))
 
 # M2. LinearRegression
-lr = LinearRegression(fit_intercept=True, n_jobs=1, normalize=False)
-lr.fit(X_m2_train, y_m2_train)
-pred_lr_train = lr.predict(X_m2_train)
-pred_lr_val = lr.predict(X_m2_val)
+lr_m2 = LinearRegression(fit_intercept=True, n_jobs=1, normalize=False)
+lr_m2.fit(X_m2_train, y_m2_train)
+pred_lr_train = lr_m2.predict(X_m2_train)
+pred_lr_val = lr_m2.predict(X_m2_val)
 test_m2_acc[('LinearRegression_MSE', 'train')] = mean_squared_error(y_m2_train, pred_lr_train)
 test_m2_acc[('LinearRegression_MSE', 'val')] = mean_squared_error(y_m2_val, pred_lr_val)
-print(lr.coef_)
+print(lr_m2.coef_)
+regression_methods.append('lr')
 
 # M2. Ridge regression
 if enable_ridge_regression:
@@ -293,6 +311,7 @@ if enable_ridge_regression:
     test_m2_acc[('Ridge_MSE', 'val')] = mean_squared_error(y_m2_val, pred_rid_val)
     print("Ridge best_score:", rid_rs.best_score_,
           "best_params:", rid_rs.best_params_)
+    regression_methods.append('rid')
 
 # M2. RandomForestRegressor
 if enable_random_forest_regressor:
@@ -308,6 +327,7 @@ if enable_random_forest_regressor:
     test_m2_acc[('RandomForestRegressor_MSE', 'val')] = mean_squared_error(y_m2_val, pred_rfr_val)
     print("RandomForestRegressor best_score:", rfr_rs.best_score_,
           "best_params:", rfr_rs.best_params_)
+    regression_methods.append('rfr')
 
 # M2. GradientBoostingRegressor
 if enable_gradient_boosting_regressor:
@@ -328,50 +348,131 @@ if enable_gradient_boosting_regressor:
         mean_squared_error(y_m2_val, pred_gbr_val)
     print("GradientBoostingRegressor best_score:", gbr_rs.best_score_,
           "best_params:", gbr_rs.best_params_)
+    regression_methods.append('gbr')
 
 res_sales_df = pd.DataFrame()
 for (alg, test_set) in test_m2_acc.keys():
     res_sales_df.loc[alg, test_set] = test_m2_acc[(alg, test_set)]
 print(res_sales_df)
 
+# Third model (M3). Direct computation of expected return
+# M3. RandomForestRegressor
+enable_random_forest_regressor_m3 = True
+test_m3_acc = {}
+if enable_random_forest_regressor_m3:
+    print('Using randomized search to tune a random forest regressor...')
+    param_dist_rfr_m3 = {"max_depth": [1, 2, 3, 4, 5]}
+    rfr_m3 = RandomForestRegressor(max_depth=3, random_state=0)
+    rfr_rs_m3 = RandomizedSearchCV(rfr_m3, n_iter=5, cv=5,
+                                param_distributions=param_dist_rfr_m3)
+    rfr_rs_m3.fit(X_m3_train, y_m3_train)
+    pred_rfr_train_m3 = rfr_rs_m3.predict(X_m3_train)
+    pred_rfr_val_m3 = rfr_rs_m3.predict(X_m3_val)
+    test_m3_acc[('RandomForestRegressor_MSE', 'train')] = mean_squared_error(y_m3_train, pred_rfr_train_m3)
+    test_m3_acc[('RandomForestRegressor_MSE', 'val')] = mean_squared_error(y_m3_val, pred_rfr_val_m3)
+    print("RandomForestRegressor best_score:", rfr_rs_m3.best_score_,
+          "best_params:", rfr_rs_m3.best_params_)
+res_sales_m3_df = pd.DataFrame()
+for (alg, test_set) in test_m3_acc.keys():
+    res_sales_m3_df.loc[alg, test_set] = test_m3_acc[(alg, test_set)]
+print(res_sales_m3_df)
 
 # Expected sales (prob of sale * estimated sale value)
-pred_m2_lr_train_all = lr.predict(X_train)
-pred_m2_lr_val_all = lr.predict(X_val)
-pred_m2_lr_test_all = lr.predict(X_test)
-
-expected_sales_train = prob_logr_train[:, 1] * pred_m2_lr_train_all
-expected_sales_val = prob_logr_val[:, 1] * pred_m2_lr_val_all
-print("expected_sales_train:\n", expected_sales_train)
-print("true sales_train:\n", target_sales_all[indices_train])
-print("expected_sales_val:\n", expected_sales_val)
-print("true sales_val:\n", target_sales_all[indices_val])
-
-promising_sales_indices_train = np.argsort(expected_sales_train)
-promising_sales_indices_val = np.argsort(expected_sales_val)
-leads_kept_train = np.rint(0.4 * len(X_train)).astype(int)
-leads_kept_val = np.rint(0.4 * len(X_val)).astype(int)
-top_promising_sales_indices_train = promising_sales_indices_train[-leads_kept_train:]
-top_promising_sales_indices_val = promising_sales_indices_val[-leads_kept_val:]
-print("top expected_sales_train:\n", expected_sales_train[top_promising_sales_indices_train])
-print("correct expected_sales_train:\n", target_sales_all[indices_train][top_promising_sales_indices_train])
-print("top expected_sales_val:\n", expected_sales_val[top_promising_sales_indices_val])
-print("correct expected_sales_val:\n", target_sales_all[indices_val][top_promising_sales_indices_val])
+# pred_m2_lr_train_all = lr_m2.predict(X_train)
+# pred_m2_lr_val_all = lr_m2.predict(X_val)
+# pred_m2_lr_test_all = lr_m2.predict(X_test)
 
 total_revenue_train = target_sales_all[indices_train].sum()
 total_revenue_val = target_sales_all[indices_val].sum()
 print("total_revenue_train", total_revenue_train)
 print("total_revenue_val", total_revenue_val)
 
-e_total_revenue_train_sel = expected_sales_train[top_promising_sales_indices_train].sum()
-e_total_revenue_val_sel = expected_sales_val[top_promising_sales_indices_val].sum()
-print("e_total_revenue_train_sel", e_total_revenue_train_sel)
-print("e_total_revenue_val_sel", e_total_revenue_val_sel)
+classification_method = 'rfc'
+regression_method = 'rfr'
+leads_kept_train = np.rint(0.40 * len(X_train)).astype(int)
+leads_kept_val = np.rint(0.40 * len(X_val)).astype(int)
 
-total_revenue_train_sel = target_sales_all[indices_train][top_promising_sales_indices_train].sum()
-total_revenue_val_sel = target_sales_all[indices_val][top_promising_sales_indices_val].sum()
-print("total_revenue_train_sel", total_revenue_train_sel)
-print("total_revenue_val_sel", total_revenue_val_sel)
+for classification_method in classification_methods:
+    if classification_method == 'rfc':
+        prob_train = prob_rf_train[:,1]
+        prob_val = prob_rf_val[:,1]
+    elif classification_method == 'logr':
+        prob_train = prob_logr_train[:,1]
+        prob_val = prob_logr_val[:,1]
+    elif classification_method == 'lr':
+        prob_train = prob_lr_train[:,1]
+        prob_val = prob_lr_val[:,1]
+    elif classification_method == 'gbc':
+        prob_train = prob_gbc_train[:, 1]
+        prob_val = prob_gbc_val[:, 1]
+    else:
+        raise ValueError('Unknown classification_method:', classification_method)
+
+    for regression_method in regression_methods:
+        if regression_method == 'lr':
+            pred_train = lr_m2.predict(X_train)
+            pred_val = lr_m2.predict(X_val)
+        elif regression_method == 'rid':
+            pred_train = rid_rs.predict(X_train)
+            pred_val = rid_rs.predict(X_val)
+        elif regression_method == 'rfr':
+            pred_train = rfr_rs.predict(X_train)
+            pred_val = rfr_rs.predict(X_val)
+        elif regression_method == 'gbr':
+            pred_train = gbr_rs.predict(X_train)
+            pred_val = bgr_rs.predict(X_val)
+        else:
+            raise ValueError('Unknown regression_method:', regression_method)
+
+        expected_sales_train = prob_train * pred_train
+        expected_sales_val = prob_val * pred_val
+
+        #print("expected_sales_train:\n", expected_sales_train)
+        #print("expected_sales_train_m3:\n", expected_sales_train_m3)
+        #print("true sales_train:\n", target_sales_all[indices_train])
+        #print("expected_sales_val:\n", expected_sales_val)
+        #print("expected_sales_val_m3:\n", expected_sales_val_m3)
+        #print("true sales_val:\n", target_sales_all[indices_val])
+
+        promising_sales_indices_train = np.argsort(expected_sales_train)
+        promising_sales_indices_val = np.argsort(expected_sales_val)
+
+        top_promising_sales_indices_train = promising_sales_indices_train[-leads_kept_train:]
+        top_promising_sales_indices_val = promising_sales_indices_val[-leads_kept_val:]
+
+        #print("top expected_sales_train:\n", expected_sales_train[top_promising_sales_indices_train])
+        #print("correct expected_sales_train:\n", target_sales_all[indices_train][top_promising_sales_indices_train])
+        #print("top expected_sales_val:\n", expected_sales_val[top_promising_sales_indices_val])
+        #print("correct expected_sales_val:\n", target_sales_all[indices_val][top_promising_sales_indices_val])
+
+        print("classification:", classification_method, "regression:", regression_method)
+        e_total_revenue_train_sel = expected_sales_train[top_promising_sales_indices_train].sum()
+        e_total_revenue_val_sel = expected_sales_val[top_promising_sales_indices_val].sum()
+        print("e_total_revenue_train_sel", e_total_revenue_train_sel)
+        print("e_total_revenue_val_sel", e_total_revenue_val_sel)
+
+        total_revenue_train_sel = target_sales_all[indices_train][top_promising_sales_indices_train].sum()
+        total_revenue_val_sel = target_sales_all[indices_val][top_promising_sales_indices_val].sum()
+        print("total_revenue_train_sel", total_revenue_train_sel)
+        print("total_revenue_val_sel", total_revenue_val_sel)
+
+
+expected_sales_train_m3 = pred_rfr_train_m3
+expected_sales_val_m3 = pred_rfr_val_m3
+promising_sales_indices_train_m3 = np.argsort(expected_sales_train_m3)
+promising_sales_indices_val_m3 = np.argsort(expected_sales_val_m3)
+top_promising_sales_indices_train_m3 = promising_sales_indices_train_m3[-leads_kept_train:]
+top_promising_sales_indices_val_m3 = promising_sales_indices_val_m3[-leads_kept_val:]
+
+e_total_revenue_train_sel_m3 = expected_sales_train_m3[top_promising_sales_indices_train_m3].sum()
+e_total_revenue_val_sel_m3 = expected_sales_val_m3[top_promising_sales_indices_val_m3].sum()
+print("e_total_revenue_train_sel_m3", e_total_revenue_train_sel_m3)
+print("e_total_revenue_val_sel_m3", e_total_revenue_val_sel_m3)
+
+total_revenue_train_sel_m3 = target_sales_all[indices_train][top_promising_sales_indices_train_m3].sum()
+total_revenue_val_sel_m3 = target_sales_all[indices_val][top_promising_sales_indices_val_m3].sum()
+print("total_revenue_train_sel_m3", total_revenue_train_sel_m3)
+print("total_revenue_val_sel_m3", total_revenue_val_sel_m3)
 
 
 # TODO: Create module for data_loading
